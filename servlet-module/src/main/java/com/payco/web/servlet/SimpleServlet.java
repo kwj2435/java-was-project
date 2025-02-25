77092ceb3c58ff4ed6568859fe59be04939a705e
김의진(한샘몰백엔드개발팀) <kimuj0035@hanssem.com>
2025-02-25 15:57:28 +0900

    최종 리팩토링

--- servlet-module/src/main/java/com/payco/web/servlet/SimpleServlet.java
+++ servlet-module/src/main/java/com/payco/web/servlet/SimpleServlet.java
@@ -5,6 +5,9 @@ import com.payco.web.http.HttpResponse;
 
 import java.io.IOException;
 
+/**
+ * 과제 스펙 6번 구현 위한 Servlet 인터페이스
+ */
 public interface SimpleServlet {
   void service(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException;
 }
